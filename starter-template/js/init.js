6385500d7667b1907f894113f9482c796e988397
Kudakwashe Chambwe <kaydee.kuda@gmail.com>
2017-03-09 13:48:47 +0100

    Javascript Carousel and Modals
    Added modals, carousel effect for showing content on page

--- starter-template/js/init.js
+++ starter-template/js/init.js
@@ -2,6 +2,10 @@
   $(function(){
 
     $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.carousel').carousel();
+    $('#modal1').modal();
+    //$('#modal1').modal('close');
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
