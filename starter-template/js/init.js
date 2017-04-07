cf7abf10e7b075239c297e86f24023af29c0983b
Kudakwashe Chambwe <kaydee.kuda@gmail.com>
2017-04-07 12:26:52 +0200

    Javascript alterations
    Small changes to the js file

--- starter-template/js/init.js
+++ starter-template/js/init.js
@@ -32,17 +32,20 @@
 
           $.getJSON("http://api.tvmaze.com/search/shows?q=" + film, function(json) {
               console.log(json);
+
               if (json.length !== 0) {
-                 // window.location.replace("search_res.html");
                   $('#finfo').html('<img style="max-height: 400px" id="filmInfo" src=' + json[0].show.image.original + ' />');
                     getRecommended(json);
+                    //return json;
+
               } else {
                 console.log('Nothing found :(');
-                $.getJSON("http://api.tvmaze.com/search/shows?q=game+of+thrones", function (json) {
+                $.getJSON("http://api.tvmaze.com/search/shows?q=game+of+thrones", function(json) {
                     console.log(json[0].show.image.original);
                     $('#finfo').html('<h5 class="center light white-text"> Found nothing. Maybe you meant...? ' +
                         '<br></h5><img id="filmInfo" src='
                         + json[0].show.image.original + ' />');
+
                 });
               }
           });
@@ -56,12 +59,13 @@
 
      var getRecommended = function(json) {
 
-          console.log("getRecommended");
+          console.log("Getting Recommended");
 
-          var films = [];
+          //var json = getFilmInfo();
+          var films = []; //the array containing the recommended films
           var filmsLen = 10;
           var cnt = 0;
-          //the array containing the recommended films
+
 
           /* Criteria declarations for finding new films */
 
@@ -74,24 +78,23 @@
 
          $.getJSON("http://api.tvmaze.com/shows", function(json) {
              console.log(json);
-             while (cnt < filmsLen) {
+             //while (cnt < filmsLen) {
                  for (var i = 0; i < 240; i++) {
                      if ((json[i].genres[0] === sgenre) && (json[i].rating.average >= score)) {
                          console.log(json[i].name + " " + json[i].genres[0] + " " + json[i].rating.average + "\n");
                          films.push(json[i]);
                          console.log(json[i]);
                          cnt++;
+
                      }
-                 }
+               //  }
              }
 
              console.log("Better: \n");
              console.log(films);
+             return films;
 
-             //for(var i = 0; i< 10; i++){
-             //    console.log(films[i]);
-                 //$('#results').load("search_res.html").html('<a class="carousel-item" href="#modal1"><img src=' + films[i] + ' ></a>');
-             //}
+
          });
       }
 
